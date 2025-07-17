0b6c7f0cb607bf61985986010d0014d63a4f97ea
Lasyasri01 <gandulasyasri@gmail.com>
2025-07-17 12:51:41 +0530

    first commit

---
+++ Big.java
@@ -0,0 +1,14 @@
+class Big{
+	public static void main(String args[])
+	{
+		  int a = Integer.parseInt(args[0]);
+	          int b = Integer.parseInt(args[1]);
+        	  int c = Integer.parseInt(args[2]);
+		if(a>b && a>c)
+			System.out.println(a);
+		else if(b>a && b>c)
+                        System.out.println(b);
+		else(c>b && c>b)
+                        System.out.println(c);
+	}
+}
